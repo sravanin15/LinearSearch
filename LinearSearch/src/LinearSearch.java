bdd6c264354b9f6e86caa727246975f6ae5b8ac2
sravanin15 <sravaninuthanapati15@gmail.com>
2021-02-02 13:00:30 +0530

    linearsearch commit

---
+++ LinearSearch/src/LinearSearch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		int size=sc.nextInt();
+		int[] array=new int[size];
+		for(int i=0;i<array.length;i++)
+		{
+			array[i]=sc.nextInt();
+		}
+		int key=sc.nextInt();
+		int index=searchElement(array,key);
+		System.out.println("Element is at index:"+index);
+	}
+	private static int searchElement(int[] array, int key) {
+		// TODO Auto-generated method stub
+		int index=0;
+		for(int i=0;i<array.length;i++)
+		{
+			if(array[i]==key) {
+				index=array[i];
+			}
+		}
+		return index;
+	}
+}
